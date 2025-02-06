b5831954ee857a3a9aa92887ef0ab52a6441096e
Itonkdong <kostadinoskiviktor@yahoo.com>
2025-02-06 13:27:29 +0100

    Added LLMAssistant

--- agent/MLAgentIO.py
+++ agent/MLAgentIO.py
@@ -58,30 +58,82 @@ class MLAgentIO:
 
     def build_starting_prompt(self) -> str:
         prompt = (f"{self.instructions["tools_description"]} "
-                  f"\n\nResearch Problem: {self.active_task.description}"
                   f"\n\n{self.instructions["introduction"]}"
                   f"\n\n{self.instructions["response_format"]}")
         return prompt
 
+    def get_research_problem(self):
+        return f"Research Problem: {self.active_task.description}"
+
 
 API_KEY = read_file("../api_keys/open_ai.txt")
+
+
+class AssistantOutput:
+
+    def __init__(self, reflection: str, research_plan: str, fact_check: str, thought: str, action: str,
+                 action_inputs: list):
+        self.research_plan = research_plan
+        self.reflection = reflection
+        self.fact_check = fact_check
+        self.thought = thought
+        self.action = action
+        self.action_inputs = action_inputs
+
+    def to_string(self):
+        return (f"Reflection:{self.reflection}\n\n"
+                f"Research Plan and Status: {self.research_plan}\n\n"
+                f"Fact Check: {self.fact_check}"
+                f"Thought: {self.thought}\n\n"
+                f"Action: {self.action}\n\n"
+                f"Action Inputs: {self.action_inputs}\n\n")
+
+
+class LLMAssistant:
+
+    def __init__(self, api_key: str, starting_instructions: str):
+        self.starting_instructions = self.to_developer_message(starting_instructions)
+        self.history = []
+        self.client = OpenAI(api_key=api_key)
+
+    @staticmethod
+    def to_developer_message(instruction: str) -> dict:
+        return {"role": "developer", "content": instruction}
+
+    @staticmethod
+    def to_user_message(message: str) -> dict:
+        return {"role": "user", "content": message}
+
+    @staticmethod
+    def to_assistant_message(response: str) -> dict:
+        return {"role": "assistant", "content": response}
+
+    def build_context(self, assistant_output: str, observation: str) -> list:
+        previous_assistant_message = self.to_assistant_message(assistant_output)
+        self.history.append(previous_assistant_message)
+        observation_message = self.to_user_message(observation)
+        context = [self.starting_instructions] + self.history + [observation_message]
+        self.history.append(observation_message)
+        return context
+
+    def __ask_assistant(self, context: list, model="gpt-4o-mini", max_tokens=None) -> str:
+        response = self.client.chat.completions.create(
+            model=model,
+            messages=context,
+            max_tokens=max_tokens,
+            n=1,
+            store=True
+        )
+
+        return response.choices[0].message.content
+
+    def ask(self, output: str, observation: str) -> str:
+        context = self.build_context(output, observation)
+        output = self.__ask_assistant(context)
+        return output
+
 
 if __name__ == '__main__':
     ml_agent_io = MLAgentIO(task_name="sarcasm_lstm")
     prompt = ml_agent_io.starting_prompt
 
-    # Testing OpenAi connection
-    client = OpenAI(api_key=API_KEY)
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[{
-            "role": "user",
-            "content": "Tell me a joke about AI"
-        }
-        ],
-        max_tokens=50,
-        n=1,
-        store=True
-    )
-
-    print(response.choices[0].message.content)
